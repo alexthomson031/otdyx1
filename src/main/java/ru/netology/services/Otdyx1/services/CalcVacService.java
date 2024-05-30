2e95092254acd267629294d3bc0b76399b252834
Netotest <athomson@mail.ru>
2024-05-30 22:53:58 +0300

    commit1

--- src/main/java/ru/netology/services/Otdyx1/services/CalcVacService.java
+++ src/main/java/ru/netology/services/Otdyx1/services/CalcVacService.java
@@ -2,9 +2,10 @@ package ru.netology.services.Otdyx1.services;
 public class CalcVacService {
     public int calculate(int income, int expenses, int threshold) {
         int bankAccount = 0;
+        int vacation = (bankAccount - expenses);
         for (int month = 1; month <= 12; month++)
             if (bankAccount > threshold) {
-                int more = (bankAccount -= expenses) - (bankAccount / 3);
+                int more = vacation - (bankAccount/3);
                 System.out.println(
                         "Месяц " + month + "." +
                                 "Денег " + bankAccount + "." +
